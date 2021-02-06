5f4763c3423e772ec9cb56bcf049ed82a07e3737
valentinac2311 <v.calderonm@uniandes.edu.co>
2021-02-05 20:29:16 -0500

    Cambios Estudiante 1

--- App/view.py
+++ App/view.py
@@ -43,6 +43,7 @@ def printMenu():
     print("1- Cargar Libros")
     print("2- Cargar Tags")
     # TO-DO: Modificación de Est-1 en el Lab 2
+    print("3- Cargar Book-Tags!")
     print("0- Salir")
 
 
@@ -64,6 +65,8 @@ def loadBookTags():
     Cargar los Tags de libros
     """
     # TO-DO: Modificación de Est-1 en el Lab 2
+    # TO-DO: Modificación de Est-1 en el Lab 2
+    return controller.loadBooks('GoodReads/book_tags-small.csv')
 
 
 """
@@ -78,6 +81,7 @@ while True:
         print('Total de libros cargados: ' + str(lt.size(books)))
 
         # TO-DO: Modificación de Est-1 en el Lab 2
+        print('Primer libro cargado: ' + str(lt.firstElement(books)))
 
 
         # TO-DO: Modificación de Est-2 en el Lab 2
